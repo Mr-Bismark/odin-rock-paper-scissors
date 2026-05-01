ac8cdc20eb3dc9a81ec5a9a3e14ee6dab0d1d725
Mr-Bismark <bismarkfaithful@gmail.com>
2026-05-01 10:42:35 +0000

    Create getComputerChoice function
        -Generates a random number
        -Assigns 'rock', 'paper' or 'scissors' based on the random number
        -Returns the computer's choice

---
+++ script.js
@@ -0,0 +1,51 @@
+/*
+Functions
+    getComputerChoice
+        generates a number and returns computer choice
+    
+    getHumanChoice
+        inputs and returns the human's choice
+    
+    playRound
+        Has computerChoice and humanChoice as arguments
+        Determines the winner from logic
+        Displays round winner
+        Increments winner's score
+
+    playGame
+        Calls the playRound Function 5x
+        displays the final score and overall winner
+    
+Variables
+    rand - stores the random number
+    choice - stores user's choice [returned by getHumanChoice]
+    option - stores the random number
+    comChoice - stores computer choice [returned by getComputerChoice]
+    humanScore - stores human's score [increases each time human wins]
+    computerScore - stores computer's score [increases each time computer wins]
+*/
+
+
+function getComputerChoice(){
+    //Generate random number
+    let rand = Math.floor(Math.random() * 3) + 1;
+
+    //Determine computer choice from random number
+    let comChoice;
+    if(rand == 1){
+        comChoice = "rock";
+    } else if (rand == 2){
+        comChoice = "paper";
+    } else{
+        comChoice = "scissors";
+    }
+
+    return comChoice;
+}
+
+function getHumanChoice(){}
+
+function playRound(){}
+
+function playGame(){}
+
